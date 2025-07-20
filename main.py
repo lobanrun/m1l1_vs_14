4d808781b1a5c1b90fa1fbb07c4837760366c1ba
lobanrun <nikitalobanov394@gmail.com>
2025-07-20 15:07:58 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "КРИНЖ": "Что-то очень странное или стыдное",
+            "ЛОЛ": "Что-то очень смешное"
+            }
+# print(meme_dict['КРИНЖ'])
+# meme_dict['СВЭГ'] = 'Что-то крутое'
+# print(meme_dict)
+# del meme_dict['ЛОЛ']
+# print(meme_dict)
+
+print(meme_dict.keys())
+print(meme_dict.values())
+print(meme_dict.items())
+word = input("Введите непонятное слово (большими буквами!): ")
